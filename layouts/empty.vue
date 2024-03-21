7eac466841d8acc483a7ba23c200fb6a40275c0b
jsmsme <chakrit7358@gmail.com>
2024-03-21 22:24:02 +0700

    Pre-Exam

---
+++ layouts/empty.vue
@@ -0,0 +1,16 @@
+<template>
+        <!-- page content go here -->
+        <div class="container mx-auto p-4">
+            <slot />
+        </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+    .router-link-exact-active {
+        color: #12b488;
+    }
+</style>
---
+++ pages/wireframe.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="center">
+      <p>(Search)</p>
+    </div>
+    <div class="container">
+        <div class="grid grid-cols-2 gap-16 mx-56 ">
+
+            <div class="border rounded-[24px] h-56 w-96 ">
+            <div class="border  m-4 h-28 text-center p-10">
+                <p>(Picture)</p>
+            </div>
+            <div class="flex  text-center h-24">
+                <div class="flex border  w-10 h-10 rounded-full mx-4 "> 
+                </div>
+                <div class="flex flex-col p-2">
+                    <hr class="w-64 h-2 p-2">
+                    <hr class="w-64 h-2 p-2">
+                    <hr class="w-64 h-2 p-2">
+                </div>
+            </div>
+        </div>
+        <div class="border  rounded-[24px] h-56 w-96 ">
+            <div class="border  m-4 h-28 text-center p-10">
+                <p>(Picture)</p>
+            </div>
+            <div class="flex  text-center h-24">
+                <div class="flex border  w-10 h-10 rounded-full mx-4 "> 
+                </div>
+                <div class="flex flex-col p-2">
+                    <hr class="w-64 h-2 p-2">
+                    <hr class="w-64 h-2 p-2">
+                    <hr class="w-64 h-2 p-2">
+                </div>
+            </div>
+        </div>
+        <div class="border  rounded-[24px] h-56 w-96 ">
+            <div class="border  m-4 h-28 text-center p-10">
+                <p>(Picture)</p>
+            </div>
+            <div class="flex  text-center h-24">
+                <div class="flex border  w-10 h-10 rounded-full mx-4 "> 
+                </div>
+                <div class="flex flex-col p-2">
+                    <hr class="w-64 h-2 p-2">
+                    <hr class="w-64 h-2 p-2">
+                    <hr class="w-64 h-2 p-2">
+                </div>
+            </div>
+        </div>
+        <div class="border  rounded-[24px] h-56 w-96 ">
+            <div class="border  m-4 h-28 text-center p-10">
+                <p>(Picture)</p>
+            </div>
+            <div class="flex  text-center h-24">
+                <div class="flex border  w-10 h-10 rounded-full mx-4 "> 
+                </div>
+                <div class="flex flex-col p-2">
+                    <hr class="w-64 h-2 p-2">
+                    <hr class="w-64 h-2 p-2">
+                    <hr class="w-64 h-2 p-2">
+                </div>
+            </div>
+        </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+  .center {
+        padding: auto;
+        width: 13%;
+        text-align: center;
+        align-content: center;
+        border: 0.3px solid;
+        margin: auto; 
+        margin-top: 15px;
+        border-radius: 5px;
+  }
+  .container { 
+        margin: 85px;
+        align-content: center;
+        justify-content: center;
+    }
+</style>
